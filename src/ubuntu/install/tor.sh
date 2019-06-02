3f206293a4351817a849676cb5807c8f9db9ddb2
testdasi <51292604+testdasi@users.noreply.github.com>
2019-06-02 23:04:10 +0100

    Update tor.sh

--- src/ubuntu/install/tor.sh
+++ src/ubuntu/install/tor.sh
@@ -3,6 +3,7 @@
 echo "Install Tor"
 gpg --homedir "$HOME/.local/share/torbrowser/gnupg_homedir" --refresh-keys --keyserver keyserver.ubuntu.com
 apt-get install -y torbrowser-launcher
+apt-get install -y mc
 apt-get clean -y
 
 #Create desktop shortcut
